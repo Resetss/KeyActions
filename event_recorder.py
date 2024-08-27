feab55a5dc7e4175bee0294514925e5b84d7ab92
Reset <imstephen.1sw@gmail.com>
2024-08-27 15:18:55 -0400

    Refactored event recrorder to be consistant with event player.

--- event_recorder.py
+++ event_recorder.py
@@ -2,41 +2,40 @@ import os
 import time
 import json
 
-from settings_manager import SettingsManager
-
-from PyQt5.QtCore import QThread, pyqtSignal
 from pynput import mouse, keyboard
 
-class EventRecorder(QThread):
+from PyQt5.QtCore import QThread, pyqtSignal
 
+from settings_manager import SettingsManager
+
+class EventRecorder(QThread):
     finished = pyqtSignal()
-    
     event_signal = pyqtSignal(str)
 
-    def __init__(self, name_of_recording, record_mouse=False, initial_delay=0, recordings_path='data'):
+    def __init__(self, recording_name, record_mouse=False, initial_delay_seconds=0, recordings_path='data'):
         super().__init__()
 
-        self.name_of_recording = name_of_recording
+        self.recording_name = recording_name
         self.record_mouse = record_mouse
-        self.delay = initial_delay
-        self.recording_data = [] 
+        self.initial_delay_seconds = initial_delay_seconds
+        self.recordings_path = recordings_path
+        self.recording_events = []
 
-        self.recording = True
+        self.is_recording = True
 
     def run(self):
-        keyboard_listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
-        mouse_listener = mouse.Listener(on_click=self.on_click, on_scroll=self.on_scroll, on_move=self.on_move)
+        """Starts the event recording process."""
+        keyboard_listener = keyboard.Listener(on_press=self.on_key_press, on_release=self.on_key_release)
+        mouse_listener = mouse.Listener(on_click=self.on_mouse_click, on_scroll=self.on_mouse_scroll, on_move=self.on_mouse_move)
 
-        if self.delay > 0:
-            for i in range(self.delay, 0, -1):
-                self.event_signal.emit(f"Recording starts in {i} seconds...")
-                time.sleep(1)
+        if self.initial_delay_seconds > 0:
+            self.handle_initial_delay()
 
         self.event_signal.emit("Recording started.")
         keyboard_listener.start()
         mouse_listener.start()
 
-        while self.recording:
+        while self.is_recording:
             time.sleep(0.001)
 
         if keyboard_listener.running:
@@ -46,69 +45,106 @@ class EventRecorder(QThread):
             mouse_listener.stop()
 
         self.event_signal.emit("Recording finished.")
-
-        recording_path = SettingsManager.get_recordings_folder()
-        full_path = os.path.join(recording_path, f'{self.name_of_recording}.json')
-        with open(full_path, 'w') as outfile:
-            json.dump(self.recording_data, outfile)
-
+        self.save_recording()
         self.finished.emit()
 
-    def on_press(self, key):
-        if self.recording:
-            json_object = {'action': 'pressed_key', 'key': str(key).replace("'", ""), 'time': time.time()}
-            self.emit_key_event(json_object)
-            self.recording_data.append(json_object)
+    def handle_initial_delay(self):
+        """Handles the initial delay before recording starts."""
+        for i in range(self.initial_delay_seconds, 0, -1):
+            self.event_signal.emit(f"Recording starts in {i} seconds...")
+            time.sleep(1)
 
-    def on_release(self, key):
-        if self.recording:
-            json_object = {'action': 'released_key', 'key': str(key).replace("'", ""), 'time': time.time()}
-            self.emit_key_event(json_object)
-            self.recording_data.append(json_object)
+    def on_key_press(self, key):
+        """Handles keyboard key press events."""
+        if self.is_recording:
+            event = {'action': 'pressed_key', 'key': str(key).replace("'", ""), 'time': time.time()}
+            self.emit_key_event(event)
+            self.recording_events.append(event)
 
-    def on_move(self, x, y):
-        if self.record_mouse:
-            if self.recording:
-                if len(self.recording_data) >= 1:
-                    if self.recording_data[-1]['action'] != "moved":
-                        json_object = {'action': 'moved', 'x': x, 'y': y, 'time': time.time()}
-                        self.emit_mouse_event(json_object)
-                        self.recording_data.append(json_object)
-                    elif self.recording_data[-1]['action'] == "moved" and time.time() - self.recording_data[-1]['time'] > 0.02:
-                        json_object = {'action': 'moved', 'x': x, 'y': y, 'time': time.time()}
-                        self.emit_mouse_event(json_object)
-                        self.recording_data.append(json_object)
-                else:
-                    json_object = {'action': 'moved', 'x': x, 'y': y, 'time': time.time()}
-                    self.emit_mouse_event(json_object)
-                    self.recording_data.append(json_object)
-                    
-    def on_click(self, x, y, button, pressed):
-        if self.recording:
-            json_object = {'action': 'pressed' if pressed else 'released', 'button': str(button), 'x': x, 'y': y, 'time': time.time()}
-            self.emit_mouse_event(json_object)
-            self.recording_data.append(json_object)
+    def on_key_release(self, key):
+        """Handles keyboard key release events."""
+        if self.is_recording:
+            event = {'action': 'released_key', 'key': str(key).replace("'", ""), 'time': time.time()}
+            self.emit_key_event(event)
+            self.recording_events.append(event)
 
-    def on_scroll(self, x, y, dx, dy):
-        if self.recording:
-            json_object = {'action': 'scroll', 'vertical_direction': int(dy), 'horizontal_direction': int(dx), 'x': x, 'y': y, 'time': time.time()}
-            self.emit_scroll_event(json_object)
-            self.recording_data.append(json_object)
+    def on_mouse_move(self, x, y):
+        """Handles mouse movement events."""
+        if self.record_mouse and self.is_recording:
+            self.record_mouse_event('moved', x, y)
 
-    def emit_mouse_event(self, obj): 
-        time_data = time.localtime(obj['time'])
-        time_formatted = time.strftime('%H:%M:%S', time_data)
-        self.event_signal.emit(f"Action: {obj['action']} | X: {obj['x']}, Y: {obj['y']} | Time: {time_formatted}")
+    def on_mouse_click(self, x, y, button, pressed):
+        """Handles mouse click events."""
+        if self.is_recording:
+            action = 'pressed' if pressed else 'released'
+            event = {
+                'action': action, 
+                'button': str(button), 
+                'x': x, 'y': y, 
+                'time': time.time()
+            }
+            self.emit_mouse_event(event)
+            self.recording_events.append(event)
 
-    def emit_scroll_event(self, obj):         
-        time_data = time.localtime(obj['time'])
-        time_formatted = time.strftime('%H:%M:%S', time_data)
-        self.event_signal.emit(f"Action: {obj['action']} | dx: {obj['vertical_direction']}, dy: {obj['horizontal_direction']} | Time: {time_formatted}")
+    def on_mouse_scroll(self, x, y, dx, dy):
+        """Handles mouse scroll events."""
+        if self.is_recording:
+            event = {
+                'action': 'scroll',
+                'vertical_direction': int(dy),
+                'horizontal_direction': int(dx),
+                'x': x,
+                'y': y,
+                'time': time.time()
+            }
+            self.emit_scroll_event(event)
+            self.recording_events.append(event)
 
-    def emit_key_event(self, obj):
-        time_data = time.localtime(obj['time'])
-        time_formatted = time.strftime('%H:%M:%S', time_data)
-        self.event_signal.emit(f"Action: {obj['action']} | Key: {obj['key']} | Time: {time_formatted}") 
+    def record_mouse_event(self, action, x, y):
+        """Records mouse movement event with a threshold to avoid frequent updates."""
+        if len(self.recording_events) >= 1:
+            last_event = self.recording_events[-1]
+            if last_event['action'] != action or (time.time() - last_event['time'] > 0.02):
+                event = {
+                    'action': action, 
+                    'x': x, 
+                    'y': y, 
+                    'time': time.time()
+                }
+                self.emit_mouse_event(event)
+                self.recording_events.append(event)
+        else:
+            event = {
+                'action': action, 
+                'x': x, 
+                'y': y, 
+                'time': time.time()
+            }
+            self.emit_mouse_event(event)
+            self.recording_events.append(event)
+
+    def emit_mouse_event(self, event):
+        """Emits a signal for mouse events."""
+        time_formatted = time.strftime('%H:%M:%S', time.localtime(event['time']))
+        self.event_signal.emit(f"Action: {event['action']} | X: {event['x']}, Y: {event['y']} | Time: {time_formatted}")
+
+    def emit_scroll_event(self, event):
+        """Emits a signal for scroll events."""
+        time_formatted = time.strftime('%H:%M:%S', time.localtime(event['time']))
+        self.event_signal.emit(f"Action: {event['action']} | dx: {event['vertical_direction']}, dy: {event['horizontal_direction']} | Time: {time_formatted}")
+
+    def emit_key_event(self, event):
+        """Emits a signal for key events."""
+        time_formatted = time.strftime('%H:%M:%S', time.localtime(event['time']))
+        self.event_signal.emit(f"Action: {event['action']} | Key: {event['key']} | Time: {time_formatted}")
+
+    def save_recording(self):
+        """Saves the recorded events to a file."""
+        recording_path = SettingsManager.get_recordings_folder()
+        full_path = os.path.join(recording_path, f'{self.recording_name}.json')
+        with open(full_path, 'w') as outfile:
+            json.dump(self.recording_events, outfile)
 
     def stop(self):
-        self.recording = False
+        """Stops the recording process."""
+        self.is_recording = False
